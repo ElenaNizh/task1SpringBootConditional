cca9818ef5be2c29dd5f36dfea4c9c4e210e14f2
ElenaNizh <lena-vn@yandex.ru>
2023-03-25 03:44:43 +0300

    initial commit

---
+++ src/main/java/ru/netology/task1SpringBoot/config/JavaConfig.java
@@ -0,0 +1,24 @@
+package ru.netology.task1SpringBoot.config;
+
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import ru.netology.task1SpringBoot.systemProfile.DevProfile;
+import ru.netology.task1SpringBoot.systemProfile.ProductionProfile;
+import ru.netology.task1SpringBoot.systemProfile.SystemProfile;
+
+@Configuration
+public class JavaConfig {
+    @Bean
+    @ConditionalOnProperty(value = "netology.profile.dev", havingValue = "true")
+    public SystemProfile devProfile() {
+        return new DevProfile();
+    }
+
+    @Bean
+    @ConditionalOnProperty(value = "netology.profile.dev", havingValue = "false")
+    public SystemProfile prodProfile() {
+        return new ProductionProfile();
+    }
+}
---
+++ src/main/java/ru/netology/task1SpringBoot/systemProfile/SystemProfile.java
@@ -0,0 +1,5 @@
+package ru.netology.task1SpringBoot.systemProfile;
+
+public interface SystemProfile {
+    String getProfile();
+}
